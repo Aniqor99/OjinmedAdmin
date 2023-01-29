17667a3c8cdab64a617269fa19270a0913df755c
Antaqor <antaqor@gmail.com>
2023-01-29 11:05:31 +0800

    Modify

--- src/api/reason/routes/reason.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * reason router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::reason.reason');
--- src/api/reason/services/reason.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * reason service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::reason.reason');
