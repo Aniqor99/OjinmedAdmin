ce3388d50cd088b5f53f6e3b8c98ad92a6ac5d8d
Antaqor <antaqor@gmail.com>
2023-04-12 16:59:54 +0800

    Modify

--- src/api/service-two/controllers/service-two.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * service-two controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::service-two.service-two');
--- src/api/service-two/routes/service-two.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * service-two router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::service-two.service-two');
